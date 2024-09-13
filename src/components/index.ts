9e914ee20bac03b2afe33c9634d3764b08146d2c
Fernanda Fuentes <fernandaplaterofuentes@gmail.com>
2024-09-12 23:06:42 -0600

    segundo proyecto

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+
+export { Sidebar } from './sidebar/Sidebar'
+export { SidebarMenuItems } from './sidebar/SidebarMenuItems'
+
+export { SimpleWidget } from './dashboard/SimpleWidget'
+export { WidgetsGrid } from './dashboard/WidgetsGrid'
